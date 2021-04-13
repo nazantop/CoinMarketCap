7fd9d48f6c8c07783aeb1b46076c3e9743cbb111
nazantop <nazantop@gmail.com>
2021-04-13 12:57:02 +0300

    Second commit

--- Coin.DATA/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Coin.DATA
-{
-    public class Class1
-    {
-    }
-}
--- Coin.HELPER/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Coin.HELPER
-{
-    public class Class1
-    {
-    }
-}
